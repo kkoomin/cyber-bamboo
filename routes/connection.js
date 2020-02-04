8fa598e6f18e002a0fbc990f5056d69b88ad8676
kkoomin <kkooming2@gmail.com>
2020-02-04 09:14:17 +0900

    Fix header

--- routes/connection.js
+++ routes/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql");
 
 const con = mysql.createConnection({
-  host: "localhost",
+  host: "70.12.113.173",
   user: "test",
   password: "test",
   database: "bamboodb",
